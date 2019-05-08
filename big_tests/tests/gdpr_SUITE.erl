766940b43a64474ea71d58a7f9a137a379c1fb07
Ludwik Bukowski <ludwik.bukowski@gmail.com>
2019-05-08 18:20:52 +0200

    Add support for vcard removal

--- big_tests/tests/gdpr_SUITE.erl
+++ big_tests/tests/gdpr_SUITE.erl
@@ -13,6 +13,8 @@
 -export([init_per_testcase/2, end_per_testcase/2]).
 -export([
          retrieve_vcard/1,
+         remove_vcard/1,
+         try_remove_data_of_non_existing_user/1,
          retrieve_roster/1,
          retrieve_mam/1,
          retrieve_offline/1,
@@ -45,7 +47,8 @@ all() ->
     [
      {group, retrieve_personal_data},
      {group, retrieve_personal_data_with_mods_disabled},
-     {group, retrieve_negative}
+     {group, retrieve_negative},
+     {group, remove_personal_data}
     ].
 
 groups() ->
@@ -77,7 +80,12 @@ groups() ->
                                                      ]},
      {retrieve_negative, [], [
                               data_is_not_retrieved_for_missing_user
-                             ]}
+                             ]},
+        {remove_personal_data, [parallel], [
+            % per type
+            try_remove_data_of_non_existing_user,
+            remove_vcard
+        ]}
     ].
 
 init_per_suite(Config) ->
@@ -112,6 +120,13 @@ init_per_testcase(retrieve_inbox = CN, Config) ->
             {skip, require_rdbms}
     end;
 init_per_testcase(retrieve_vcard = CN, Config) ->
+    case vcard_update:is_vcard_ldap() of
+        true ->
+            {skip, skipped_for_simplicity_for_now}; % TODO: Fix the case for LDAP as well
+        _ ->
+            escalus:init_per_testcase(CN, Config)
+    end;
+init_per_testcase(remove_vcard = CN, Config) ->
     case vcard_update:is_vcard_ldap() of
         true ->
             {skip, skipped_for_simplicity_for_now}; % TODO: Fix the case for LDAP as well
@@ -165,6 +180,8 @@ pubsub_required_modules() ->
 %% -------------------------------------------------------------
 
 %% ------------------------- Data retrieval - per type verification -------------------------
+try_remove_data_of_non_existing_user(Config) ->
+    {"Error: \"User does not exist\"\n", 1} = ejabberdctl("remove_personal_data", [<<"user_not_exist">>, <<"localhost">>], Config).
 
 retrieve_vcard(Config) ->
     escalus:fresh_story(Config, [{alice, 1}], fun(Alice) ->
@@ -184,6 +201,17 @@ retrieve_vcard(Config) ->
             maybe_stop_and_unload_module(mod_vcard, mod_vcard_backend, Config),
             retrieve_and_validate_personal_data(
               Alice, Config, "vcard", ExpectedHeader, ExpectedItems)
+        end).
+
+remove_vcard(Config) ->
+    escalus:fresh_story(Config, [{alice, 1}], fun(Alice) ->
+        AliceFields = [{<<"FN">>, <<"Alice">>}, {<<"LN">>, <<"Ecila">>}],
+        AliceSetResultStanza
+            = escalus:send_and_wait(Alice, escalus_stanza:vcard_update(AliceFields)),
+        escalus:assert(is_iq_result, AliceSetResultStanza),
+
+        {0, _} = remove_personal_data(Alice, Config),
+        retrieve_and_validate_personal_data(Alice, Config, "vcard", ["jid", "vcard"], [])
         end).
 
 retrieve_roster(Config) ->
@@ -503,6 +531,12 @@ retrieve_personal_data(User, Domain, Config) ->
     Filename = random_filename(Config),
     {CommandOutput, Code} = ejabberdctl("retrieve_personal_data", [User, Domain, Filename], Config),
     {Filename, Code, CommandOutput}.
+
+remove_personal_data(Client, Config) ->
+    User = escalus_client:username(Client),
+    Domain = escalus_client:server(Client),
+    {CommandOutput, Code} = ejabberdctl("remove_personal_data", [User, Domain], Config),
+    {Code, CommandOutput}.
 
 random_filename(Config) ->
     TCName = atom_to_list(?config(tc_name, Config)),
